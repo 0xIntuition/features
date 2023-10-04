6beaf880b61eed2c60b83b3182a11243b690f1f5
Kara <kara.graysen@gmail.com>
2023-10-04 11:07:51 -0400

    Update install.sh

--- src/foundry/install.sh
+++ src/foundry/install.sh
@@ -7,3 +7,5 @@ curl -L https://foundry.paradigm.xyz | bash
 /root/.foundry/bin/foundryup
 
 cp -r /root/.foundry /home/vscode
+
+echo "export PATH=\"/home/vscode/.foundry/bin:$PATH\"" >> /home/vscode/.bashrc
