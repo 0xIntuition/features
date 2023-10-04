a5c4398c8382209c80c4118ecab47acf4afece73
Kara <kara.graysen@gmail.com>
2023-10-04 10:46:30 -0400

    Update install.sh

--- src/foundry/install.sh
+++ src/foundry/install.sh
@@ -4,9 +4,6 @@ set -e
 echo "Activating feature 'foundry'"
 
 curl -L https://foundry.paradigm.xyz | bash
-cat /root/.bashrc
-ls /usr/local/bin/.foundry/bin/
-
-/home/vscode/.foundry/bin/foundryup
+/root/.foundry/bin/foundryup
 
 # chmod +x /usr/local/bin/foundry
